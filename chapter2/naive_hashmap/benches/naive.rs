f9e8604dc2c91352e9253d65c58cf22598864236
mihaimaganu17 <maganu_gx_mihai@yahoo.com>
2025-03-24 08:14:54 +0100

    Fixed benches to newer edition of tooling

--- chapter2/naive_hashmap/benches/naive.rs
+++ chapter2/naive_hashmap/benches/naive.rs
@@ -1,20 +1,20 @@
-use rand::SmallRng;
+use rand::{rngs::SmallRng, SeedableRng, Rng};
 
 fn insert_and_lookup_naive(mut count: u64) {
     // SmallRng is Xoshiro256++. Insecure but fast
-    let mut rng = SmallRng::from_seed([1981, 1986, 2003, 2011]);
+    let mut rng = SmallRng::seed_from_u64(1981 | (1986 << 8) | (2003 << 16) | (2011 << 24));
     // Create a new hashmap
-    let mut hashmap = naive_hashmap::HashMap::new();
+    let mut hash_map = naive_hashmap::HashMap::new();
 
     while count != 0 {
         // Get a random key
-        let key = rng.gen::<u32>();
+        let key = rng.random::<u32>();
         // Arbitrarily generate an insert or o lookup
-        if rng.get::<bool>() {
-            let value = rng.get::<u32>();
+        if rng.random::<bool>() {
+            let value = rng.random::<u32>();
             hash_map.insert(key, value);
         } else {
-            hash_map.get(key);
+            hash_map.get(&key);
         }
         count -= 1;
     }
@@ -22,20 +22,22 @@ fn insert_and_lookup_naive(mut count: u64) {
 
 fn insert_and_lookup_standard(mut count: u64) {
     // SmallRng is Xoshiro256++. Insecure but fast
-    let mut rng = SmallRng::from_seed([1981, 1986, 2003, 2011]);
+    let mut rng = SmallRng::seed_from_u64(1981 | (1986 << 8) | (2003 << 16) | (2011 << 24));
     // Create a new hashmap
-    let mut hashmap = std::collections::HashMap::new();
+    let mut hash_map = std::collections::HashMap::new();
 
     while count != 0 {
         // Get a random key
-        let key = rng.gen::<u32>();
+        let key = rng.random::<u32>();
         // Arbitrarily generate an insert or o lookup
-        if rng.get::<bool>() {
-            let value = rng.get::<u32>();
+        if rng.random::<bool>() {
+            let value = rng.random::<u32>();
             hash_map.insert(key, value);
         } else {
-            hash_map.get(key);
+            hash_map.get(&key);
         }
         count -= 1;
     }
 }
+
+fn main() {}

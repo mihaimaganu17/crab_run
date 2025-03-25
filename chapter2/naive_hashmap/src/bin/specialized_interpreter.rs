11c97d5a5b49c5765e87056b862a39133e83b9e5
mihaimaganu17 <maganu_gx_mihai@yahoo.com>
2025-03-25 10:19:09 +0100

    Specialized interpreter

---
+++ chapter2/naive_hashmap/src/bin/specialized_interpreter.rs
@@ -0,0 +1,42 @@
+use naive_hashmap::HashMapU8;
+use std::io::{self, BufRead};
+
+fn main() {
+    // Get a handle to the standard input
+    let stdin = io::stdin();
+
+    // Create a new HashMap
+    let mut hash_map = HashMapU8::new();
+
+    for line in stdin.lock().lines().map_while(Result::ok) {
+        let mut cmd = line.split(" ");
+        match cmd.next() {
+            // If we are requested a lookup
+            Some("LOOKUP") => {
+                // If we have a key
+                if let Some(key) = cmd.next() {
+                    if let Ok(key_u8) = u8::from_str_radix(key, 10) {
+                        // Get the key
+                        let _ = hash_map.get(&key_u8);
+                    } else {
+                        continue;
+                    }
+                } else {
+                    continue;
+                }
+            }
+            Some("INSERT") => {
+                // If we have a key and a value
+                if let (Some(key), Some(value)) = (cmd.next(), cmd.next()) {
+                    if let Ok(key_u8) = u8::from_str_radix(key, 10) {
+                        // Insert the value and key
+                        let _ = hash_map.insert(key_u8, value.to_string());
+                    }
+                } else {
+                    continue;
+                }
+            }
+            Some(_) | None => continue,
+        }
+    }
+}
